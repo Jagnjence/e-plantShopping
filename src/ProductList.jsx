e83a0dfe4009c226ecdc0408ad8ef38c638f8966
Vladan <vladan9999@yahoo.com>
2025-04-14 09:13:21 -0400

    initial commit

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -212,6 +212,10 @@ function ProductList({ onHomeClick }) {
             ]
         }
     ];
+    const styleImg = {
+        width: '200px',
+        height: '200px',
+    }
     const styleObj = {
         backgroundColor: '#4CAF50',
         color: '#fff!important',
@@ -272,10 +276,36 @@ function ProductList({ onHomeClick }) {
                     <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
                 </div>
             </div>
+            
             {!showCart ? (
                 <div className="product-grid">
-
-
+                    <table className="table_item_data">
+                        
+                        {plantsArray.map((item, index) => (
+                        
+                        <tbody>
+                            <tr key={index}><td>PLANT CATEGORY{item.category}</td></tr>
+                            <tr>
+                                <td>PLANT NAME</td>
+                                <td>PLANT DESCRIPTION</td>
+                                <td>PLANT COST</td>
+                                <td>PLANT IMAGE</td>
+                            </tr>
+                                {item.plants.map((item) => (
+                                        <tr>
+                                            <td>{item.name}</td>
+                                            <td>{item.description}</td>
+                                            <td>{item.cost}</td>
+                                            
+                                                <div className="img" style={styleImg}>
+                                                        <img src={item.image} alt={item.name} />
+                                                </div>
+                                            
+                                        </tr>
+                                    ))}            
+                        </tbody>        
+                    ))}
+                    </table>
                 </div>
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
